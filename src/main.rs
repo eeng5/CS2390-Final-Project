e957f5b2f49863c6a92013946a1f7f7907502c77
Eleanor Eng <eleanor_eng@brown.edu>
2021-10-25 14:20:30 -0400

    CRUD api with version errors

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+// #![feature(plugin)]
+// #![plugin(rocket_codegen)]
+//
+// extern crate rocket;
+
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate diesel;
+extern crate r2d2;
+extern crate r2d2_diesel;
+
+// use serde_json::Value;
+use rocket_contrib::json::{Json, JsonValue};
+
+mod db;
+mod schema;
+
+mod hero;
+use hero::{Hero};
+
+#[post("/", data = "<hero>")]
+fn create(hero: Json<Hero>, connection: db::Connection) -> Json<Hero> {
+    let insert = Hero { id: None, ..hero.into_inner() };
+    Json(Hero::create(insert, &connection))
+}
+
+#[get("/")]
+fn read(connection: db::Connection) -> Json<Value> {
+    Json(json!(Hero::read(&connection)))
+}
+
+#[put("/<id>", data = "<hero>")]
+fn update(id: i32, hero: Json<Hero>, connection: db::Connection) -> Json<Value> {
+    let update = Hero { id: Some(id), ..hero.into_inner() };
+    Json(json!({
+        "success": Hero::update(id, update, &connection)
+    }))
+}
+
+#[delete("/<id>")]
+fn delete(id: i32, connection: db::Connection) -> Json<Value> {
+    Json(json!({
+        "success": Hero::delete(id, &connection)
+    }))
+}
+
+// #[get("/")]
+// fn index() -> &'static str {
+//     "Hello, world!"
+// }
+
+fn main() {
+    rocket::ignite()
+        .manage(db::connect())
+        .mount("/hero", routes![create, update, delete])
+        .mount("/heroes", routes![read])
+        .launch();
+}
